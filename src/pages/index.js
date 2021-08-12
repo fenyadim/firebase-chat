8f56e24b3cd53c74e943ae94f12efbb870b8c989
fenyadim <dimos9813@gmail.com>
2021-08-12 12:21:58 +0800

    Chat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,3 @@
-import React from "react";
-
-// export const Chat = React.lazy(() => import('./Chat/Chat'))
 export {default as Chat} from './Chat/Chat'
 export {default as Login} from './Login/Login'
 export {default as Registration} from './Registration/Registration'
